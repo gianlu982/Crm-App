4e4e858703e107d8e3b5752f5209599e30471ddd
Gianlu <nobile.gianluca@gmail.com>
2022-06-20 19:22:55 +0200

    login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Operator } from 'src/assets/interface/operator'; 
+import { Router } from '@angular/router';
 import operatorList from 'src/assets/operatorList.json'; // This is a json file that contains the list of operators
+import loginOperator from 'src/assets/loginOperator.json'; // This is a json file that contains the list of user e passwwords
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +12,14 @@ import operatorList from 'src/assets/operatorList.json'; // This is a json file 
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   operatorlog: Operator[] = [];
 
   operator: Operator[]  = operatorList;
 
   operatorsLogged: string = "";
+  inputPassword : string = "";
 
   ngOnInit(): void {
   }
@@ -29,9 +33,14 @@ export class LoginComponent implements OnInit {
     localStorage.setItem('operatorLogged', JSON.stringify(this.operatorlog));
   }
 
-  saveOperatorsLogged(){
-    this.getOperatorsLogged();
-    this.setOperatorsLogged();
+  login(){
+    const loginObj = loginOperator.find((operator:any) => operator.id === this.operatorsLogged);
+    if(this.inputPassword === loginObj.password){
+      this.getOperatorsLogged();
+      this.setOperatorsLogged();
+      this.router.navigate(['/home']);
+    }else{
+      alert("Id or password incorrect; use Id: 101 for admin, 102,103 for user; password: 123");
+    }
   }
-
 }
--- src/assets/interface/operator.ts
+++ src/assets/interface/operator.ts
@@ -1,6 +1,5 @@
 export interface Operator {
     name: string;
     id: string;
-    password: string;
     level: string;
 }
